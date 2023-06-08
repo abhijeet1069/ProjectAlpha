7d528c1efc5b05e92b21e322b92923ae14049990
abhijeet1069 <abhijeet1069@gmail.com>
2023-06-08 21:02:11 +0530

    new commit

--- Tutorial/NatureOfCode/1-vectors/bouncing-ball/src/js/app.js
+++ Tutorial/NatureOfCode/1-vectors/bouncing-ball/src/js/app.js
@@ -1,42 +1,58 @@
 import Vector3D from './vector3D.js'
 import {getRandomInteger} from './helper.js'
 
-/*
-Vector Motion : 
-Acceleration is the rate of change of velocity.
+/**
+A force is a vector that causes an object with mass to accelerate.
 
-Acceleration Algorithms:
-    A constant acceleration
-    A totally random acceleration
-    Acceleration towards the mouse
+Newton's Law of Motion:
+    Newton's first law:
+    An object at rest stays at rest and an object in motion stays in motion.
+        In our processing world, An object's PVector velocity will remain constant if it is in a
+    state of equilibrium.
 
-*/
+    Newton's second law:
+        Force equals mass times acceleration.
+
+    Newton's third law:
+    For every action there is an equal and opposite reaction.
+        If we calculate a PVector f that is a force of object A on object B, we must also
+    apply the force PVector.mult(f,-1) that B exerts on object A.
+
+    Mass - It is a measure of the amount of matter in the object. Measured in KG
+    Weight - Force of gravity on an object. Measured in Newtons.
+    Density - Amount of mass per unit of volume. Measured in grams per centimeter.
+
+    In the world of Processing,
+    Assumption : Suppose, all our objects have a mass equal to 1.
+    So, F = ma , Vector F = Vector a
+ */
 
 class Mover{
-    constructor(){
-        this.location = new Vector3D(canvas.width/2,canvas.height/2); //z value is automatically assigned to 0
-        this.velocity = new Vector3D();
-        this.acceleration = new Vector3D();
-        this.radius = 10;
-        this.topSpeed = 5;
+    constructor(initX, initY){
+        this.location = new Vector3D(); //z value is automatically assigned to 0
+        this.location.x = initX || canvas.width/2;
+        this.location.y = initY || canvas.height/2;
+        this.velocity = new Vector3D(0,0);
+        this.acceleration = new Vector3D(0,0);
+        this.mass = 1;
     }
 
-    update(){
-        var dir =  mouse.sub(this.location);
-        this.acceleration = dir.unit();
-        this.acceleration = this.acceleration.mult(0.2);
-        
+    applyForce(force){
+        let f = force.mult(1/this.mass); //larger the mass, smaller the force
+        this.acceleration = this.acceleration.add(f);
+    }
+
+    update(){    
         this.velocity = this.velocity.add(this.acceleration);
-        this.velocity = this.velocity.limit(this.topSpeed);
-       // console.log(this.velocity.toString());
         this.location = this.location.add(this.velocity);
+        this.acceleration = this.acceleration.mult(0); //clear acceleration in each frame
     }
 
     checkEdges(){
-        if(this.location.x+this.radius > canvas.width || this.location.x-this.radius < 0){
+        if(this.location.x > canvas.width || this.location.x < 0){
             this.velocity.x = this.velocity.x * -1;
         }
-        if(this.location.y+this.radius > canvas.height || this.location.y-this.radius < 0){
+        if(this.location.y> canvas.height || this.location.y< 0){
             this.velocity.y = this.velocity.y * -1;
         }
     }
@@ -44,7 +60,7 @@ class Mover{
     display(){
         ctx.fillStyle='red';
         ctx.beginPath();
-        ctx.ellipse(this.location.x,this.location.y,this.radius,this.radius,0,0,Math.PI*2);
+        ctx.ellipse(this.location.x,this.location.y,this.mass*16,this.mass*16,0,0,Math.PI*2);
         ctx.closePath();
         ctx.fill();
         //ctx.stroke();
@@ -54,33 +70,26 @@ class Mover{
 //global variables
 var canvas; //get canvas element
 var ctx; //get 2D context
-var location; //2D vector for location
-var velocity; //2D vector for velocity
-var mover;
-var mouse = new Vector3D(); //to prevent 
-
-function getMouseCoordinates(event){
-    const rect = canvas.getBoundingClientRect();
-    const mouseX = event.clientX - rect.left;
-    const mouseY = event.clientY - rect.top;
-    return new Vector3D(mouseX,mouseY);
-}
+var mover1;
+var mover2; 
 
 function main(){
     canvas = document.getElementById("myCanvas");
     ctx = canvas.getContext("2d");
-    canvas.addEventListener('mousemove',function(event){
-        mouse = getMouseCoordinates(event);
-    });
-    mover = new Mover();
+    mover1 = new Mover();
     draw();
 }
 
 function draw(){
     ctx.clearRect(0,0,canvas.width,canvas.height); //background(255);
-    mover.update();
-    //mover.checkEdges();
-    mover.display();
+    let wind = new Vector3D(0.01,0);
+    let gravity = new Vector3D(0,0.1*mover1.mass); //gravitational force depends of object's mass
+    mover1.applyForce(wind);
+    mover1.applyForce(gravity);
+
+    mover1.update();
+    mover1.display();
+    mover1.checkEdges();
     requestAnimationFrame(draw);
 }
 
--- Tutorial/NatureOfCode/1-vectors/evolution/motion101-velocity/bouncing-ball/src/js/app.js
+++ Tutorial/NatureOfCode/1-vectors/evolution/motion101-velocity/bouncing-ball/src/js/app.js
@@ -1,63 +1,62 @@
-import Vector3D from './vector3D.js'
-import {getRandomInteger} from './helper.js'
+package com.example.javafxdemo;
 
 /*
-Motion 101
-    1. Add velocity to location
-    2. Draw object at location
+Stage, Scene, Scene-Graph(Root-Node, Branch-Node, Leaf Node)
+* */
 
-*/
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.stage.Stage;
+import javafx.util.Duration;
 
-class Mover{
-    constructor(){
-        this.location = new Vector3D(getRandomInteger(0,canvas.width),getRandomInteger(0,canvas.height),0);
-        this.velocity = new Vector3D(1,2,0);
+public class Main extends Application {
+
+    private static final int WIDTH = 800;
+    private static final int HEIGHT = 600;
+    private static final int BALL_RADIUS = 20;
+    private static int BALL_SPEED_X = 2;
+    private static int BALL_SPEED_Y = 3;
+
+
+    @Override
+    public void start(Stage primaryStage) {
+        Circle ball = new Circle(BALL_RADIUS, Color.BLUE);
+        ball.relocate(WIDTH / 2, HEIGHT / 2);
+
+        Pane root = new Pane(ball);
+        Scene scene = new Scene(root, WIDTH, HEIGHT);
+
+        primaryStage.setTitle("Bouncing Ball");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+        animateBall(ball);
     }
 
-    update(){
-        this.location = this.location.add(this.velocity);
+    private void animateBall(Circle ball) {
+        Timeline animation = new Timeline(new KeyFrame(Duration.millis(16), event -> {
+            // Move the ball
+            ball.setLayoutX(ball.getLayoutX() + BALL_SPEED_X);
+            ball.setLayoutY(ball.getLayoutY() + BALL_SPEED_Y);
+
+            // Check boundaries and bounce
+            if (ball.getLayoutX() <= 0 || ball.getLayoutX() >= WIDTH) {
+                BALL_SPEED_X = -BALL_SPEED_X; // Reverse horizontal direction
+            }
+            if (ball.getLayoutY() <= 0 || ball.getLayoutY() >= HEIGHT) {
+                BALL_SPEED_Y = -BALL_SPEED_Y; // Reverse vertical direction
+            }
+        }));
+        animation.setCycleCount(Timeline.INDEFINITE);
+        animation.play();
     }
 
-    checkEdges(){
-        if(this.location.x > canvas.width || this.location.x < 0){
-            this.velocity.x = this.velocity.x * -1;
-        }
-        if(this.location.y > canvas.height || this.location.y < 0){
-            this.velocity.y = this.velocity.y * -1;
-        }
-    }
-
-    display(){
-        ctx.fillStyle='red';
-        ctx.beginPath();
-        ctx.ellipse(this.location.x,this.location.y,10,10,0,0,Math.PI*2);
-        ctx.closePath();
-        ctx.fill();
-        //ctx.stroke();
+    public static void main(String[] args) {
+        Application.launch(args);
     }
 }
-
-//global variables
-var canvas; //get canvas element
-var ctx; //get 2D context
-var location; //2D vector for location
-var velocity; //2D vector for velocity
-var mover;
-
-function main(){
-    canvas = document.getElementById("myCanvas");
-    ctx = canvas.getContext("2d");
-    mover = new Mover()
-    //console.log(location.x);
-    draw();
-}
-
-function draw(){
-    ctx.clearRect(0,0,canvas.width,canvas.height); //background(255);
-    mover.update();
-    mover.checkEdges();
-    mover.display();
-    requestAnimationFrame(draw);
-}
-
-main();
